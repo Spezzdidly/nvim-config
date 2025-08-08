f60af680eed43fa5022ecb158412a386c6f4211d
Spezzdidly <s.johns3404@gmail.com>
2025-08-08 00:15:03 -0400

    Added c/cpp clangd lsp and removed lua_ls since it was redundant with the use of stylua. Also stylua is more customizable.

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,28 +1,33 @@
 return {
     {
-	"williamboman/mason.nvim",
-	config = function()
-	    require("mason").setup()
-	end
+        "williamboman/mason.nvim",
+        config = function()
+            require("mason").setup()
+        end,
     },
     {
-	"williamboman/mason-lspconfig.nvim",
-	config = function()
-	    require("mason-lspconfig").setup({
-		ensure_installed = { "lua_ls", "ts_ls" }
-	    })
-	end
+        "williamboman/mason-lspconfig.nvim",
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = { "ts_ls", "clangd" },
+            })
+        end,
     },
     {
-	"neovim/nvim-lspconfig",
-	config = function()
-	    local lspconfig = require("lspconfig")
-	    lspconfig.ts_ls.setup({})
-	    lspconfig.lua_ls.setup({})
+        "neovim/nvim-lspconfig",
+        config = function()
+            local lspconfig = require("lspconfig")
+            lspconfig.ts_ls.setup({})
+	    lspconfig.clangd.setup({})
 
-	    vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-	    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-	    vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
-	end
-    }
+            vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+            vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+            vim.keymap.set(
+                { "n", "v" },
+                "<leader>ca",
+                vim.lsp.buf.code_action,
+                {}
+            )
+        end,
+    },
 }
